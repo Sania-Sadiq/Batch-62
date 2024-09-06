27e1872f396089db0c3543508ffe863121750232
Sania-Sadiq <saniasadiq012@gmail.com>
2024-09-06 21:19:46 +0500

    Add files via upload

---
+++ Firstprogramme.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+print("Sania")
+print("Sania Sadiq")
+print("123")
+print("Moral of the story is:\n Union is strength.")
+print("Generative \t AI")
+print("January","February","March",sep=',')
+print("7","8","6",sep='-')
+print("Lovely",end=" ")
+print("Lahore")
+print("1",end="")
+print("2")
+print("According to research,\"mitochondria\"is the powerhouse of the cell.")
+print("backslash\\ is the escape character.")
+age=20
+print(f"I am {age} years old.")
+number=100
+print(f"{number} is three digit number.")
+for x in range (1,6):
+    print(x)
+
